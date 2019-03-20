26f16c43b36851ae33bed0c81fe5f351cb3fc7f6
LPLLEU <38284033+lplleu@users.noreply.github.com>
2019-03-20 19:34:22 +0200

    Create aladin.java

---
+++ aladin.java
@@ -0,0 +1,23 @@
+// first attempt at running a method
+// 20_March_19
+// jedenfalls
+
+public class aladin{
+
+   public static void main(String[] args){
+   
+      checkBox();
+      System.out.println(s(5));
+   
+   }   
+   
+   public static void checkBox(){
+   
+      System.out.println("mook");
+         
+   }
+   
+   public static int s(int value){
+      return value;
+   }
+}
