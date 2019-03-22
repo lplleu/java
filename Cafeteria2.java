3fe070279a9cecf82e2db7329d73080dd5fb0874
LPLLEU <38284033+lplleu@users.noreply.github.com>
2019-03-22 20:22:01 +0200

    Create Cafeteria2.java
    update of Cafeteria class (only 1 if statement), though this still doesnt catch range errors.
    32 lines.

---
+++ Cafeteria2.java
@@ -0,0 +1,32 @@
+// restaurant.java
+// 22_March_2019
+// jedenfalls
+
+import java.util.Scanner;
+
+public class Cafeteria2{
+
+   public static void main(String[] args){
+      //declare and initialize variables
+      int mealType;
+      double amountDue;
+      double[] prices ={20.00,50.00,40.00};
+      
+      Scanner con = new Scanner(System.in);
+      
+      System.out.println("How many meals? indicate 0 up to 3.");
+      
+      mealType = con.nextInt();
+      
+      if(mealType==3)
+         amountDue = 0.7*(prices[0]+prices[1]+prices[2]);
+      else
+         amountDue = prices[mealType];
+         
+      //what about entries out of range??
+         
+      //display output   
+      System.out.printf("P%.2f due.",amountDue);   
+   
+   }//ends main method
+}//ends class Cafeteria
