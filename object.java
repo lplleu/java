4efb358014c19175f52b06d32121892f515b3f26
LPLLEU <38284033+lplleu@users.noreply.github.com>
2019-04-08 00:04:22 +0200

    Update object.java

--- object.java
+++ object.java
@@ -4,8 +4,27 @@
 
 public class object{
   public static void main(String[] args){
-  
     System.out.println("helloWorld");
   }
 
+}
+    
+    Robot r1 = new Robot("Jerry","blue",49);
+
+    r1.introduceSelf();
+
+class Robot{
+    String name, color;
+    int weight;
+  
+    Robot(String n, String c, int w){
+      this.name = n;
+      this.color= c;
+      this.weight=w;  
+    }
+  
+void introduceSelf(){
+  System.out.println(
+    "My name is " + this.name
+                    );
 }
