9830e09f36fc28a3c9722db1585217ce8568ebf3
LPLLEU <38284033+lplleu@users.noreply.github.com>
2019-04-16 22:53:56 +0200

    Create CMS.java

---
+++ CMS.java
@@ -0,0 +1,42 @@
+/*
+   CourseManagementSystem.java
+   16_April_2019
+   jedenfalls
+*/
+
+import javax.swing.JOptionPane;
+
+public class CMS{
+
+   public static void main(String[] args){
+      
+      String courseCode, courseTitle;
+      int total;
+         ///courseCode = JOptionPane.showInputDialog("enter course code");      
+      total = 2;//JOptionPane.showInputDialog("how many courses?");
+        
+      for(int count=1;count<=total;count++){
+
+         courseCode = JOptionPane.showInputDialog("enter course code");
+         
+         courseTitle = JOptionPane.showInputDialog("enter course title");
+         
+         Course c1 = new Course(courseCode,courseTitle);
+         
+         JOptionPane.showMessageDialog( null, c1.getCode() + ": " + c1.getName() + " has been created.");
+         
+      }      
+         
+         // System.out.println(total + " courses created.\n");
+         
+   } // ends method
+
+} // ends class CMS
+
+
+/*
+   pending
+      -accept whole sentence
+      -accept total
+      -tell me how many courses were created
+*/
