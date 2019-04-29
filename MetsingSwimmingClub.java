98118d6156ec187ae3dd5fb9200a0d7c393f01c7
LPLLEU <38284033+lplleu@users.noreply.github.com>
2019-04-29 16:02:39 +0200

    Update MetsingSwimmingClub.java

--- MetsingSwimmingClub.java
+++ MetsingSwimmingClub.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner = sc= new Scanner(System.in);
-    string name;
+    Scanner sc= new Scanner(System.in);
+    String name;
     int experience, age, galas;
 
     System.out.println("name?");
