c63badc586915e1fe4056dfb670f5e6539c75ba4
LPLLEU <38284033+lplleu@users.noreply.github.com>
2019-05-16 20:00:13 +0200

    Create FinalExamPattern.java

---
+++ FinalExamPattern.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+   public class FinalExamPattern{   
+      public static void main(String[] args){
+      int size = 5; int i = 0;
+      int j = 0;
+      
+      for(i=0;i<size;i++){
+         for(j=0;j<size;j++){
+         if(j==i){
+            System.out.print("* ");
+         }else if((j==size-1)&&i>=(size/2)){
+            System.out.print("* ");
+         }else if(i==(size-1)&&(j>=(size/2))){
+            System.out.print("* ");
+         }else{
+            System.out.print("0 ");
+         }
+        }
+         System.out.println();
+      }
+      
+     }// ends
+   }// ends
